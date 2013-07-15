e8396628523ffd25e00a06bc534d531cfde40c77
nbuttarelli <nbuttarelli@1cb3d604-1b6f-4fed-b72b-130653c20f90>
2013-07-15 14:56:26 +0000

    Updated DB

---
+++ DB/Adding Notification Permissions to CA y CC.sql
@@ -0,0 +1,32 @@
+INSERT 	INTO UsersGroupsPermissions (UsersGroupsID,`Type`,Name,Access,`Action`) 
+			VALUES(5,'Module','Projects',1,'Access');
+			
+INSERT 	INTO UsersGroupsPermissions (UsersGroupsID,`Type`,Name,Access,`Action`) 
+			VALUES(7,'Module','Projects',1,'Access');
+
+INSERT 	INTO UsersGroupsPermissions (UsersGroupsID,`Type`,Name,Access,`Action`) 
+			VALUES(2,'Module','MyAccount',1,'Save');
+INSERT 	INTO UsersGroupsPermissions (UsersGroupsID,`Type`,Name,Access,`Action`) 
+			VALUES(3,'Module','MyAccount',1,'Save');
+INSERT 	INTO UsersGroupsPermissions (UsersGroupsID,`Type`,Name,Access,`Action`) 
+			VALUES(4,'Module','MyAccount',1,'Save');
+INSERT 	INTO UsersGroupsPermissions (UsersGroupsID,`Type`,Name,Access,`Action`) 
+			VALUES(5,'Module','MyAccount',1,'Save');
+INSERT 	INTO UsersGroupsPermissions (UsersGroupsID,`Type`,Name,Access,`Action`) 
+			VALUES(6,'Module','MyAccount',1,'Save');
+INSERT 	INTO UsersGroupsPermissions (UsersGroupsID,`Type`,Name,Access,`Action`) 
+			VALUES(7,'Module','MyAccount',1,'Save');
+INSERT 	INTO UsersGroupsPermissions (UsersGroupsID,`Type`,Name,Access,`Action`) 
+			VALUES(8,'Module','MyAccount',1,'Save');
+INSERT 	INTO UsersGroupsPermissions (UsersGroupsID,`Type`,Name,Access,`Action`) 
+			VALUES(9,'Module','MyAccount',1,'Save');
+INSERT 	INTO UsersGroupsPermissions (UsersGroupsID,`Type`,Name,Access,`Action`) 
+			VALUES(10,'Module','MyAccount',1,'Save');
+INSERT 	INTO UsersGroupsPermissions (UsersGroupsID,`Type`,Name,Access,`Action`) 
+			VALUES(11,'Module','MyAccount',1,'Save');
+INSERT 	INTO UsersGroupsPermissions (UsersGroupsID,`Type`,Name,Access,`Action`) 
+			VALUES(12,'Module','MyAccount',1,'Save');
+INSERT 	INTO UsersGroupsPermissions (UsersGroupsID,`Type`,Name,Access,`Action`) 
+			VALUES(13,'Module','MyAccount',1,'Save');
+INSERT 	INTO UsersGroupsPermissions (UsersGroupsID,`Type`,Name,Access,`Action`) 
+			VALUES(14,'Module','MyAccount',1,'Save');
